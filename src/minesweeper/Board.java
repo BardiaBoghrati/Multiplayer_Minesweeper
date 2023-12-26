3142e2d583cb360d3f830ca7c70ec73de81bc853
bardia <bardia.boghrati@gmail.com>
2023-12-26 15:35:17 -0800

    Spec and test cases for board

--- src/minesweeper/Board.java
+++ src/minesweeper/Board.java
@@ -4,12 +4,96 @@
 package minesweeper;
 
 /**
- * TODO: Specification
+ * A minesweeper board is a grid of squares. Each square is either flagged, dug,
+ * or untouched. Each square also contains a bomb or does not contain a bomb.
+ * <p>
+ * The axis of the board is defined with (x,y) coordinates starting at (0,0) in
+ * the top-left corner, extending horizontally to the right in the x-direction,
+ * and vertically down in the y-direction.
+ * <p>
+ * (x,y) coordinate of a square is within bounds if both x and y are greater
+ * than or equal to 0 and less than the size of the board.
  */
 public class Board {
-    
+
     // TODO: Abstraction function, rep invariant, rep exposure, thread safety
-    
+
     // TODO: Specify, test, and implement in problem 2
     
+    /**
+     * Initialize a board from a bombs grid. A board of equals size to bombs
+     * will be created with all squares untouched. An square x,y will contain a
+     * bomb if only if bombs contains a 1 in the same location.
+     * 
+     * @param bombs
+     *            A grid of newline-separated rows of space-separated 1s and 0s
+     *            representing the location of bombs in a board. A location
+     *            contains a bomb if and only if it contains a 1.
+     */
+    public Board(String bombs) {
+        
+    }
+
+    /**
+     * If x,y are out of bounds, the board's state remains unchanged. If x,y are
+     * in bounds, dig square x,y if untouched, otherwise leave the state as is.
+     * <p>
+     * Digging square x,y expands recursively through all neighboring squares if
+     * none of those squares contain a bomb.
+     * <p>
+     * A dug square will have a count of it's neighboring squares with bombs.
+     * 
+     * @param x
+     *            the x-coordinate of the square in the board.
+     * @param y
+     *            the y-coordinate of the square in the board.
+     */
+    public void dig(int x, int y) {
+        throw new RuntimeException("not implemented");
+    }
+
+    /**
+     * If x,y are out of bounds, the boards' state remains unchanged. If x,y are
+     * in bounds, flag square x,y if untouched, otherwise leave the state as is.
+     * 
+     * @param x
+     *            the x-coordinate of the square in the board.
+     * @param y
+     *            the y-coordinate of the square in the board.
+     */
+    public void flag(int x, int y) {
+        throw new RuntimeException("not implemented");
+    }
+
+    /**
+     * If x,y are out of bounds, the boards' state remains unchanged. If x,y are
+     * in bounds, deflag square x,y if flagged, otherwise leave the state as is.
+     * 
+     * @param x
+     *            the x-coordinate of the square in the board.
+     * @param y
+     *            the y-coordinate of the square in the board.
+     */
+    public void deflag(int x, int y) {
+        throw new RuntimeException("not implemented");
+    }
+
+    /**
+     * Returns a grid of newline-separated rows of space space-separated
+     * characters, representing the board's state with exactly one character per
+     * each square. The mapping of characters is as follows:
+     * <ul>
+     * <li>"-" denotes an untouched square.</li>
+     * <li>"F" denotes a flagged square.</li>
+     * <li>" " (space) denotes a dug square with 0 neighbors that have a bomb.
+     * </li>
+     * <li>an integer between 1 and 8 denotes a dug square with that many
+     * neighbor that have a bomb.</li>
+     * </ul>
+     */
+    @Override
+    public String toString() {
+        throw new RuntimeException("not implemented");
+    }
+
 }
--- test/minesweeper/BoardTest.java
+++ test/minesweeper/BoardTest.java
@@ -3,6 +3,8 @@
  */
 package minesweeper;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 /**
@@ -11,6 +13,12 @@ import org.junit.Test;
 public class BoardTest {
     
     // TODO: Testing strategy
+    //  A square may have 0, 1, 2, 3, 5, 8 neighbors
+    //  A grid may be a single square, linear (one-dimensional along x or y),
+    // rectangular (two dimensional with unequal size of x and y), square (two dimensional with equal sides).
+    //  A dig on may expand through any of the untouched neighbors, we can categorize the direction of the expansion 
+    // into left, right, top, bottom, top-left, top-right, bottom-left, bottom-right
+    //  We can also partition a dig operation based on the distance of the expansion: 0 (no expansion), 1, 2, and so on
     
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
@@ -18,5 +26,378 @@ public class BoardTest {
     }
     
     // TODO: Tests
+    @Test
+    public void testFlagUntouchedSqaure() {
+        Board board = new Board("0\n");
+        
+        board.flag(0, 0);
+        
+        assertEquals("F\n", board.toString()); 
+    }
     
+    @Test
+    public void testFlagFlaggedSqaure() {
+        Board board = new Board("0\n");
+        board.flag(0, 0);
+        
+        board.flag(0, 0);
+        
+        assertEquals("F\n", board.toString()); 
+    }
+    
+    @Test
+    public void testFlagDugSqaure() {
+        Board board = new Board("0\n");
+        board.dig(0, 0);
+        
+        board.flag(0, 0);
+        
+        assertEquals(" \n", board.toString()); 
+    }
+    
+    @Test
+    public void testDeflagUntouchedSqaure() {
+        Board board = new Board("0\n");
+        
+        board.deflag(0, 0);
+        
+        assertEquals("-\n", board.toString()); 
+    }
+    
+    @Test
+    public void testDeflagFlaggedSqaure() {
+        Board board = new Board("0\n");
+        board.flag(0, 0);
+        
+        board.deflag(0, 0);
+        
+        assertEquals("-\n", board.toString()); 
+    }
+    
+    @Test
+    public void testDeflagDugSqaure() {
+        Board board = new Board("0\n");
+        board.dig(0, 0);
+        
+        board.deflag(0, 0);
+        
+        assertEquals(" \n", board.toString()); 
+    }
+    
+    @Test
+    public void testDigDuggedSqaure() {
+        Board board = new Board("0 0\n");
+        board.flag(1, 0);
+        board.dig(0, 0);
+        board.deflag(1, 0);
+        
+        board.dig(0, 0);
+        
+        assertEquals("  -\n", board.toString()); 
+    }
+    
+    @Test
+    public void testDigFlaggedSquare() {
+        Board board = new Board("0\n");
+        board.flag(0, 0);
+        
+        board.dig(0, 0);
+        
+        assertEquals("F\n", board.toString());  
+    }
+    
+    @Test
+    public void testDigSqaureWithFlaggedMinedSquare() {
+        Board board = new Board("1 0 0\n");
+        board.flag(0, 0);
+        
+        board.dig(1, 0);
+        
+        assertEquals("F 1 -\n", board.toString());  
+    }
+    
+    @Test
+    public void testDigSqaureWithNoNeighbors() {
+        Board board = new Board("0\n");
+        
+        board.dig(0, 0);
+        
+        assertEquals(" \n", board.toString());
+    }
+    
+//    @Test
+//    public void testDigSquareWithFlaggedNeighbors() {
+//        Board board = new Board("0 0 0\n"+
+//                                "0 0 0\n"+
+//                                "0 0 0\n");
+//        board.flag(0, 1);
+//        board.flag(1, 0);
+//        board.flag(1, 1);
+//        
+//        board.dig(2, 2);
+//        
+//        assertEquals("- F  \n"+
+//                     "F F  \n"+
+//                     "     \n", board.toString());
+//        
+//        board.deflag(1, 1);
+//        
+//        assertEquals("- F  \n"+
+//                     "F -  \n"+
+//                     "     \n", board.toString());
+//        
+//        board.dig(1, 1);
+//        
+//        assertEquals("  F  \n"+
+//                     "F    \n"+
+//                     "     \n", board.toString());
+//    }
+    
+    @Test
+    public void testDigSquareWithFlaggedNeighbors() {
+        Board board;
+        
+        //Expansion walled in by flagged neighbors
+        board = new Board("0 0\n"+
+                          "0 0\n"+
+                          "0 0\n");
+        board.flag(0, 1);
+        board.flag(1, 1);
+        
+        board.dig(0, 0);
+        
+        assertEquals("   \n"+
+                     "F F\n"+
+                     "- -\n", board.toString());
+        
+        //Expansion not walled in by flagged neighbors
+        board = new Board("0 0\n"+
+                          "0 0\n"+
+                          "0 0\n");
+        board.flag(1, 1);
+        
+        board.dig(0, 0);
+        
+        assertEquals("   \n"+
+                     "  F\n"+
+                     "   \n", board.toString());
+    }
+    
+//    @Test
+//    public void testDigSquareWithMinedNeighbor() {
+//        Board board = new Board("1 0 0\n"+
+//                                "0 0 0\n"+
+//                                "0 0 0\n");
+//        
+//        board.dig(1, 1);
+//        
+//        assertEquals("- - -\n"+
+//                     "- 1 -\n"+
+//                     "- - -\n", board.toString());
+//        
+//        board.dig(2, 2);
+//        
+//        assertEquals("- 1  \n"+
+//                     "1 1  \n"+
+//                     "     \n", board.toString());
+//    }
+    
+    @Test
+    public void testDigSqaureDistanceFromMinedSquare() {
+        Board board;
+        
+        //Dig square on top of a bomb
+        board = new Board("1 0 0\n");
+        
+        board.dig(0, 0);
+        
+        assertEquals("     \n", board.toString());
+        
+        //Dig square one square to a bomb
+        board = new Board("1 0 0\n");
+        
+        board.dig(1, 0);
+        
+        assertEquals("- 1 -\n", board.toString());
+        
+        //Dig square two squares from a bomb
+        board = new Board("1 0 0\n");
+        
+        board.dig(2, 0);
+        
+        assertEquals("- 1  \n", board.toString());
+    }
+    
+    @Test
+    public void testDigHorizontalExpansion() {
+        Board board = new Board("0 0 0\n");   
+
+        board.dig(1, 0);
+        
+        assertEquals("     \n", board.toString());
+    }
+    
+    @Test
+    public void testDigVerticalExpansion() {
+        Board board = new Board("0\n"+
+                                "0\n"+
+                                "0\n");   
+        board.dig(0, 1);
+        
+        assertEquals(" \n"+
+                     " \n"+
+                     " \n", board.toString());
+    }
+    
+    @Test
+    public void testDigDiagonalExpansion() {
+        Board board = new Board("0 0 0\n"+
+                                "0 0 0 \n"+
+                                "0 0 0\n");   
+        board.flag(1, 0);
+        board.flag(1, 2);
+        board.flag(0, 1);
+        board.flag(2, 1);
+        
+        board.dig(1, 1);
+        
+        assertEquals("  F  \n"+
+                     "F   F\n"+
+                     "  F  \n", board.toString());
+    }
+    
+    
+    @Test
+    public void testDigMinedSquares() {
+        Board board = new Board("1 1\n"+
+                                "1 1\n");
+        
+        board.dig(0, 0);
+        
+        assertEquals("3 -\n"+
+                     "- -\n", board.toString());
+        
+        board.dig(1, 1);
+        
+        assertEquals("2 -\n"+
+                     "- 2\n", board.toString());
+        
+        board.dig(1, 0);
+        
+        assertEquals("1 1\n"+
+                     "- 1\n", board.toString());
+        
+        board.dig(0, 1);
+        
+        assertEquals("   \n"+
+                     "   \n", board.toString());
+    }
+    
+//    @Test
+//    public void testDigSquareWithDugNeighbors() {
+//        Board board = new Board("1 1 0\n"+
+//                                "1 1 0\n"+
+//                                "0 0 0\n");
+//        
+//        board.dig(0, 1);
+//        board.dig(1, 0);
+//        board.dig(1, 1);
+//        
+//        assertEquals("- 1 -\n"+
+//                     "1 1 -\n"+
+//                     "- - -\n", board.toString());
+//        
+//        board.dig(0, 0);
+//        
+//        assertEquals("    -\n"+
+//                     "    -\n"+
+//                     "- - -\n", board.toString());
+//    }
+    
+    @Test
+    public void testDigSquareWithDugNeighbor() {
+        Board board = new Board("1 1 0\n");
+        board.dig(1, 0);
+        
+        board.dig(0, 0);
+        
+        assertEquals("    -\n", board.toString());
+    }
+    
+    @Test
+    public void testDigMiddleSquare() {
+        Board board = new Board("0 0 0\n"+
+                                "0 0 0\n"+
+                                "0 0 0\n");
+        
+        board.dig(1, 1);
+        
+        assertEquals("     \n"+
+                     "     \n"+
+                     "     \n", board.toString());
+    }
+    
+    @Test
+    public void testDigBoundarySquares() {
+        Board board = new Board("0 0 0\n"+
+                                "0 1 0\n"+
+                                "0 0 0\n");
+        
+        board.dig(0, 0);
+        
+        assertEquals("1 - -\n"+
+                     "- - -\n"+
+                     "- - -\n", board.toString());
+        board.dig(1, 0);
+        
+        assertEquals("1 1 -\n"+
+                     "- - -\n"+
+                     "- - -\n", board.toString());
+        board.dig(2, 0);
+        
+        assertEquals("1 1 1\n"+
+                     "- - -\n"+
+                     "- - -\n", board.toString());
+        board.dig(2, 1);
+        
+        assertEquals("1 1 1\n"+
+                     "- - 1\n"+
+                     "- - -\n", board.toString());
+        board.dig(2, 2);
+        
+        assertEquals("1 1 1\n"+
+                     "- - 1\n"+
+                     "- - 1\n", board.toString());
+        board.dig(1, 2);
+        
+        assertEquals("1 1 1\n"+
+                     "- - 1\n"+
+                     "- 1 1\n", board.toString());
+        
+        board.dig(0, 2);
+        
+        assertEquals("1 1 1\n"+
+                     "- - 1\n"+
+                     "1 1 1\n", board.toString());
+        
+        board.dig(0, 1);
+        
+        assertEquals("1 1 1\n"+
+                     "1 - 1\n"+
+                     "1 1 1\n", board.toString());
+    }
+    
+    @Test
+    public void testDigWithFlagInTheMiddle() {
+        Board board = new Board("0 0 0\n"+
+                                "0 0 0\n"+
+                                "0 0 0\n");
+        board.flag(1, 1);
+        
+        board.dig(2, 2);
+        
+        assertEquals("     \n"+
+                     "  F  \n"+
+                     "     \n", board.toString());
+    }
 }
